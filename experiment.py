1716bc3ce68c8d05fb54dd6950da4e9d79e7cc2c
varikakasandor <varikakasandor@gmail.com>
2021-10-09 13:02:52 +0100

    Non-constant threshold dp added

--- experiment.py
+++ experiment.py
@@ -24,6 +24,33 @@ def dp_two_thinning_constant_threshold(loads_tuple, chosen, threshold):
         return res/(n*n)
 
 
+@functools.lru_cache(maxsize=400000)
+def dp_two_thinning(loads_tuple, chosen):
+    loads=list(loads_tuple)
+    if sum(loads)==n:
+        return max(loads)
+
+    accept=0
+    loads[chosen]+=1
+    for i in range(n):
+        accept+=dp_two_thinning(tuple(loads), i)
+    loads[chosen]-=1
+    accept/=n
+
+    reject=0
+    for i in range(n):
+        loads[i]+=1
+        for j in range(n):
+            reject+=dp_two_thinning(tuple(loads), j)
+        loads[i]-=1
+    reject/=(n*n)
+
+
+    return min(accept, reject)
+
+
 if __name__=="__main__":
     for threshold in range(n+1):
-        print(f"With threshold {threshold} the expected maximum load is {dp_two_thinning_constant_threshold(tuple([0]*n),0,threshold)}")
+        print(f"With {n} balls, {n} bins and constant threshold {threshold} the expected maximum load is {dp_two_thinning_constant_threshold(tuple([0]*n),0,threshold)}")
+
+    print(f"With {n} balls, {n} bins the best achievable expected maximum load is {dp_two_thinning(tuple([0]*n),0)}")
