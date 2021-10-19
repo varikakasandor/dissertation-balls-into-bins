fd0c5b05904d9a885fd5c4fcc37ce7f0669e7124
varikakasandor <varikakasandor@gmail.com>
2021-10-20 00:20:04 +0100

    n-m bug fixed

--- two_thinning/RL/two_thinning_RL.py
+++ two_thinning/RL/two_thinning_RL.py
@@ -1,11 +1,11 @@
 import random
 
-n=50
-m=250
+n=2
+m=100
 episodes=10000
 epsilon=0.1
 
-def reward(threshold): #Should be imported from two_thinning_simulation
+def reward(threshold, n=n, m=m): #Should be imported from two_thinning_simulation
     loads=[0]*n
     for _ in range(m):
         chosen=random.randrange(n)
@@ -18,15 +18,15 @@ def reward(threshold): #Should be imported from two_thinning_simulation
 
 
 def train(n=n, m=m, episodes=episodes, epsilon=epsilon):
-    q=[0]*(n+1)
-    cnt=[0]*(n+1)
+    q=[0]*(m+1)
+    cnt=[0]*(m+1)
     for _ in range(episodes):
         r=random.random()
         if r<epsilon:
-            a=random.randrange(n+1)
+            a=random.randrange(m+1)
         else:
             a=q.index(min(q))
-        r=reward(a)
+        r=reward(a,n,m)
         cnt[a]+=1
         q[a]+=(r-q[a])/cnt[a]
     
