60ed6187017e8f1590f88b9f852fe3bf27a8f29c
varikakasandor <varikakasandor@gmail.com>
2021-10-19 23:00:41 +0100

    Folder restructuring

--- experiment.py
+++ experiment.py
@@ -1,3 +1,3 @@
-from stable_baselines3 import A2C
-
-model = A2C('MlpPolicy', 'CartPole-v1').learn(10000)
+#from stable_baselines3 import A2C
+import torch
+#model = A2C('MlpPolicy', 'CartPole-v1').learn(10000)
