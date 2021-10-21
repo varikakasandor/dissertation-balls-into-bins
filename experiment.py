ed9253db435b170c0df2b1a30129c01cef0dcf95
varikakasandor <varikakasandor@gmail.com>
2021-10-21 12:50:49 +0100

    First model trained and saved - always chooses 1 as the threshold

--- experiment.py
+++ experiment.py
@@ -1,1 +1,34 @@
-from .mathematical_results import one_choice_maths
+import torch
+import numpy as np
+
+from two_thinning.RL.two_thinning_models import TwoThinningNet
+
+n=10
+m=n
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = TwoThinningNet(n,m)
+model.to(device).double()
+model.load_state_dict(torch.load('two_thinning/RL/saved_models/best.pth'))
+model.eval()
+
+eval_episodes=3
+
+
+max_loads=[]
+for _ in range(eval_episodes):
+    loads=np.zeros(n)
+    for i in range(m):
+        options=model(torch.from_numpy(loads).double())
+        print(f"The options are: {options}")
+        a=torch.argmax(options)
+        print(f"With load vector {loads}, the model chooses a threshold {a}")
+        randomly_selected=np.random.randint(n)
+        if loads[randomly_selected]<=a:
+            loads[randomly_selected]+=1
+        else:
+            loads[np.random.randint(n)]+=1
+    max_loads.append(np.max(loads))
+
+avg_max_load=sum(max_loads)/len(max_loads)
+print(avg_max_load)
--- two_thinning/RL/two_thinning_full_knowledge_RL.py
+++ two_thinning/RL/two_thinning_full_knowledge_RL.py
@@ -11,56 +11,63 @@ m=n
 
 alpha=0.1
 epsilon=0.1 #TODO: set (exponential) decay
-train_episodes=100
-eval_episodes=100
-
-model=TwoThinningNet(n,m,device)
-model.to(device).double()
-optimizer = torch.optim.Adam(model.parameters())
-MSE_loss = nn.MSELoss()
-
+train_episodes=300
+eval_episodes=300
+repetition=20
+best_performance=np.inf
 
 def epsilon_greedy(loads, epsilon=epsilon):
     action_values=model(loads)
     r=torch.rand(1)
     if r<epsilon:
-        a=torch.randint(len(action_values), (1,))
+        a=torch.randint(len(action_values), (1,))[0]
     else:
         a=torch.argmax(action_values)
-
     return a, action_values[a]
 
-for _ in range(train_episodes):
-    loads=np.zeros(n)
-    for i in range(m):
-        a,old_val=epsilon_greedy(torch.from_numpy(loads).double())
-        randomly_selected=np.random.randint(n)
-        if loads[randomly_selected]<=a:
-            loads[randomly_selected]+=1
-        else:
-            loads[np.random.randint(n)]+=1
-        
-        if i==m-1:
-            new_val=torch.from_numpy(np.array([-np.max(loads)])).to(device)
-        else:
-            _, new_val=epsilon_greedy(torch.from_numpy(loads).double())
-            new_val=new_val.detach()
+for _ in range(repetition):
 
-        loss = MSE_loss(old_val, new_val)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    model=TwoThinningNet(n,m,device)
+    model.to(device).double()
+    optimizer = torch.optim.Adam(model.parameters())
+    MSE_loss = nn.MSELoss()
 
-max_loads=[]
-for _ in range(eval_episodes):
-    loads=np.zeros(n)
-    for i in range(m):
-        a,old_val=epsilon_greedy(torch.from_numpy(loads).double(),0)
-        randomly_selected=np.random.randint(n)
-        if loads[randomly_selected]<=a:
-            loads[randomly_selected]+=1
-        else:
-            loads[np.random.randint(n)]+=1
-    max_loads.append(np.max(loads))
 
-print(sum(max_loads)/len(max_loads))
+    for _ in range(train_episodes):
+        loads=np.zeros(n)
+        for i in range(m):
+            a,old_val=epsilon_greedy(torch.from_numpy(loads).double())
+            randomly_selected=np.random.randint(n)
+            if loads[randomly_selected]<=a:
+                loads[randomly_selected]+=1
+            else:
+                loads[np.random.randint(n)]+=1
+            
+            if i==m-1:
+                new_val=torch.as_tensor(-np.max(loads)).to(device)
+            else:
+                _, new_val=epsilon_greedy(torch.from_numpy(loads).double())
+                new_val=new_val.detach()
+
+            loss = MSE_loss(old_val, new_val)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+    max_loads=[]
+    for _ in range(eval_episodes):
+        loads=np.zeros(n)
+        for i in range(m):
+            a=torch.argmax(model(torch.from_numpy(loads).double()))
+            randomly_selected=np.random.randint(n)
+            if loads[randomly_selected]<=a:
+                loads[randomly_selected]+=1
+            else:
+                loads[np.random.randint(n)]+=1
+        max_loads.append(np.max(loads))
+
+    avg_max_load=sum(max_loads)/len(max_loads)
+    if avg_max_load<best_performance:
+        torch.save(model.state_dict(), 'two_thinning/RL/saved_models/best.pth')
+        best_performance=avg_max_load
+    print(avg_max_load)
