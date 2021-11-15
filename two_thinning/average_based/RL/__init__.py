1f452bba7117dcf4edd695b00ff16df2a816db47
varikakasandor <varikakasandor@gmail.com>
2021-11-15 22:14:02 +0000

    Minor folder restructuring

--- two_thinning/average_based/RL/__init__.py
+++ two_thinning/average_based/RL/__init__.py
@@ -1,1 +1,1 @@
-from two_thinning.average_based.RL import bandit_neuralnet_RL, basic_neuralnet_RL, invalid_RL, replay_neuralnet_RL
+from two_thinning.average_based.RL import bandit_neuralnet_RL, basic_neuralnet_RL, invalid_RL, DQN
