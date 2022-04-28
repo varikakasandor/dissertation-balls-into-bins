ef845519cd4d4e1aea58d0f1ac3edd442fb2d7cb
varikakasandor <varikakasandor@gmail.com>
2022-04-28 17:21:25 +0100

    Gitignore updated

---
+++ evaluation/graphical_two_choice/create_statistics.py
@@ -0,0 +1,53 @@
+from os.path import exists
+
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+
+from helper.helper import flatten
+from k_choice.graphical.two_choice.graphs.complete_graph import CompleteGraph
+from k_choice.graphical.two_choice.graphs.cycle import Cycle
+from k_choice.graphical.two_choice.graphs.hypercube import HyperCube
+
+GMS = ((Cycle(4), 25), (HyperCube(4), 25), (CompleteGraph(4), 25),
+       (Cycle(16), 50), (HyperCube(16), 50), (CompleteGraph(16), 50),
+       (Cycle(32), 32), (HyperCube(32), 32), (CompleteGraph(32), 32))
+
+STRATEGIES = ("greedy", "random", "local_reward_optimiser", "dp", "dqn")
+
+
+def calculate_statistics(graph, m, strategy, alpha=0.95):
+    read_path = f"data/{graph.name}_{graph.n}_{m}_{strategy}.csv"
+    if exists(read_path):
+        df = pd.read_csv(read_path)
+        scores = np.array(df["score"].to_list())
+        mean = np.mean(scores)
+        sem = st.sem(scores)
+        if sem > 0:
+            lower, upper = st.norm.interval(alpha=alpha, loc=mean, scale=sem)
+            return mean, (upper - lower) / 2
+        else:
+            return mean, 0
+    else:
+        return -1, -1
+
+
+def create_csv(gms=GMS, strategies=STRATEGIES):
+    cols = flatten([[f"mean_{graph.name}_{graph.n}_{m}", f"confidence_{graph.name}_{graph.n}_{m}"] for graph, m in gms])
+    vals = []
+    for strategy in strategies:
+        row = []
+        for graph, m in gms:
+            mean, confidence = calculate_statistics(graph=graph, m=m, strategy=strategy)
+            row.extend([mean, confidence])
+
+        vals.append(row)
+
+    df = pd.DataFrame(data=vals, columns=cols, index=strategies)
+    output_path = f"data/comparison.csv"
+    df.to_csv(output_path)
+    return df
+
+
+if __name__ == "__main__":
+    create_csv()
--- helper/csv_to_latex.py
+++ helper/csv_to_latex.py
@@ -27,4 +27,4 @@ def csv_to_latex(inp_path, out_path, precision=2):
 if __name__ == "__main__":
     csv_to_latex("../evaluation/two_thinning/data/comparison.csv", "../evaluation/two_thinning/data/comparison.tex")
     csv_to_latex("../evaluation/k_thinning/data/comparison.csv", "../evaluation/k_thinning/data/comparison.tex")
-    #csv_to_latex("../evaluation/graphical_two_choice/data/comparison.csv", "../evaluation/graphical_two_choice/data/comparison.tex")
+    csv_to_latex("../evaluation/graphical_two_choice/data/comparison.csv", "../evaluation/graphical_two_choice/data/comparison.tex")
